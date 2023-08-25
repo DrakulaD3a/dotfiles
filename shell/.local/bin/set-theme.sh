18968584b603f166feb0cc19808bab1084bc0a21
Lukas Strnad <strnad.lukas@proton.me>
2023-08-25 20:13:51 +0200

    Better switching between themes

--- shell/.local/bin/set-theme.sh
+++ shell/.local/bin/set-theme.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-time=$(date +%H)
+options=" dark\n light"
 
-if [[ $time -lt 17 && $time -ge 8 ]]; then
+chosen=$(echo -e "$options" | wofi -d -i -p "Theme")
+
+if [[ $chosen == " light" ]]; then
     ~/.local/bin/light-gtk.sh
     swww img -t none ~/personal/pictures/wallpapers/light-wallpaper.jpg
     ~/.local/bin/reload-waybar.sh
-    echo "light"
 else 
     ~/.local/bin/gtk.sh
     swww img -t none ~/personal/pictures/wallpapers/dark-wallpaper.jpg
     ~/.local/bin/reload-waybar.sh
-    echo "dark"
 fi
