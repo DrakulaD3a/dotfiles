c852b2ce6b655b12033bf55a83c3c9df65beacc3
Lukas Strnad <strnad.lukas@proton.me>
2024-04-21 20:25:38 +0200

    rofi

--- shell/.local/bin/connect_windows.sh
+++ shell/.local/bin/connect_windows.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xfreerdp -grab-keyboard /v:192.168.122.241 /u:lukas /p:ABCabc123 /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive
+xfreerdp3 -grab-keyboard /v:192.168.122.241 /u:lukas /p:ABCabc123 /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive
