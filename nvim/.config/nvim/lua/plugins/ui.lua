596362968e351ef68d22c68c60ef4ec7ceb7e914
Lukas Strnad <strnad.lukas@proton.me>
2024-12-06 17:50:28 +0100

    feat: Using snacks.nvim

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,55 @@
 return {
     {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            bigfile = {
+                enable = true,
+                notify = true,
+                size = 1024 * 1024, -- 1MiB
+            },
+            notifier = {
+                enable = true,
+                timeout = 3000,
+            },
+            quickfile = { enable = true },
+            git = { enable = true },
+            lazygit = { enable = true },
+            dashboard = { enable = true },
+        },
+        keys = {
+            {
+                "<leader>gg",
+                function()
+                    Snacks.lazygit()
+                end,
+                desc = "Lazygit",
+            },
+            {
+                "<leader>gb",
+                function()
+                    Snacks.git.blame_line()
+                end,
+                desc = "Git Blame Line",
+            },
+            {
+                "<leader>nn",
+                function()
+                    Snacks.notifier.hide()
+                end,
+                desc = "Dismiss All Notifications",
+            },
+            {
+                "<leader>nh",
+                function()
+                    Snacks.notifier.show_history()
+                end,
+                desc = "Notification History",
+            },
+        },
+    },
+    {
         "folke/zen-mode.nvim",
         config = true,
     },
