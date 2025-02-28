b47cb36c64d4754ce9a9f4013e6127238a80da61
Lukas Strnad <strnad.lukas@proton.me>
2025-02-28 16:10:19 +0100

    fix: Windows run script

--- shell/.local/bin/open_windows.sh
+++ shell/.local/bin/open_windows.sh
@@ -2,4 +2,4 @@
 
 virsh --connect qemu:///system start win11
 sleep 30
-xfreerdp3 -grab-keyboard /v:192.168.122.241 /u:lukas /p:ABCabc123 /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive
+xfreerdp3 -grab-keyboard /v:192.168.122.241 /u:lukas /p:ABCabc123 /size:100% /d: /dynamic-resolution
